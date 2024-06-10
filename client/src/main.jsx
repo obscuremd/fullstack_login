4d606da538f8ff8c62bce7942d8e0f8ae7be67b3
dan <md.erhenede@gmail.com>
2024-06-10 20:34:43 +0100

    removed secrets

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RecoilRoot } from 'recoil'
+import { app } from './firebaseconfig.jsx'
+
+app
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
